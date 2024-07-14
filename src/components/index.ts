f608b2cd3d373af7176b8dfac252b6cab58d3371
EverardoNava <everardo.nava8942@alumnos.udg.mx>
2024-07-14 08:49:54 -0600

    Pantalla del producto

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,9 +5,9 @@ export * from "./products/product-grid/ProductGridItem";
 
 export * from "./product/slideshow/ProductSlideshow";
 export * from "./product/slideshow/ProductMovileSlideshow";
-
 export * from "./product/quantity-selector/QuantitySelector";
 export * from "./product/size-selector/SizeSelector";
+export * from "./product/stock-label/StockLabel";
 
 export * from "./ui/sidebar/Sidebar";
 export * from "./ui/pagination/Pagination";
